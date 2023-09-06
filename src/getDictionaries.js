f492f67b11c722f1f5840882794164b9e51d4293
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-09-05 21:34:09 -0400

    Added Serverless

---
+++ src/getDictionaries.js
@@ -0,0 +1,11 @@
+const sendResponse = require('./common')
+const { dictionaries } = require('../utils/constants')
+
+const handler = async () => {
+    try {
+        return sendResponse(200, { dictionaries: dictionaries })
+    } catch (error) {
+        return sendResponse(400, { message: 'Error: ' + error })
+    }
+}
+module.exports = { handler }
---
+++ src/getReferences.js
@@ -0,0 +1,11 @@
+const sendResponse = require('./common')
+const { references } = require('../utils/constants')
+
+const handler = async () => {
+    try {
+        return sendResponse(200, { references: references })
+    } catch (error) {
+        return sendResponse(400, { message: 'Error: ' + error })
+    }
+}
+module.exports = { handler }
